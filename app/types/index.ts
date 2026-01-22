6b40f2702241a30c817364f99daa653e635767bc
ImrenR <imrenrahbay@gmail.com>
2026-01-22 20:46:15 +0000

    types/index.ts

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,28 +1,28 @@
-export enum Role{
-  ADMIN="ADMIN",
-  MANAGER="MANAGER",
-  USER="USER",
-  GUEST="GUEST",
+export enum Role {
 
-}
+USER="USER",
+GUEST="GUEST",
+MANAGER="MANAGER",
+ADMIN="ADMIN",
+};
 
-export interface User{
-id: string;
-name:string;
-email:string;
-role:Role;
-teamId?:string; // when we create the user, the user might not assigned on any team, so optional
-team?:Team;
-createdAt: Date;
-updatedAt: Date;
-
+export interface User {
+  id: string,
+  name: string,
+  email: string,
+  role:Role,
+  teamId?: string,
+  team:Team,
+  createdAt: Date,
+  updatedAt: Date,
 }
 
 export interface Team{
-  id:string;
-  name:string;
-  description?: string | null;
-  members:User[];
-  createdAt: Date;
-  updatedAt: Date;
+id:string,
+name:string,
+description?:string,
+code:string,
+members: User[],
+createdAt: Date,
+updatedAt: Date,
 }
