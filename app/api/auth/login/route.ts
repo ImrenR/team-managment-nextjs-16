f04f49801c50aa2b93f0eac58405c0e329786034
ImrenR <imrenrahbay@gmail.com>
2026-01-23 01:07:16 +0000

    update

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,6 +1,5 @@
 import { generateToken, verifyPassword } from "@/app/lib/auth";
 import { prisma } from "@/app/lib/db";
-import { Role } from "@/app/types";
 import { NextRequest, NextResponse } from "next/server";
 
 
